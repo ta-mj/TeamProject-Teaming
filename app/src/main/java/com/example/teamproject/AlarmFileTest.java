086b7aa96154844dceb1a764825c03bcb2a62870
KCS2183 <ckdtlr2000@naver.com>
2022-10-24 01:36:46 +0900

    수정한 사항 - 알람파일테스트.java 수정

--- app/src/main/java/com/example/teamproject/AlarmFileTest.java
+++ app/src/main/java/com/example/teamproject/AlarmFileTest.java
@@ -1,19 +1,30 @@
 package com.example.teamproject;
 
+import static android.graphics.Color.RED;
+import static android.os.Build.VERSION_CODES.O;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContract;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -32,8 +43,12 @@ public class AlarmFileTest extends AppCompatActivity {
     private Button fileButton;
     private Button alarmButton;
     private Button saveButton;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
     private TextView filenameView;
     private File savedFile;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,7 +57,7 @@ public class AlarmFileTest extends AppCompatActivity {
         fileButton = findViewById(R.id.file_button);
         fileButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view) {//ㅎ
                 Toast.makeText(getApplicationContext(), "파일 버튼 클릭!", Toast.LENGTH_SHORT).show();
                 Intent openfile = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                 openfile.addCategory(Intent.CATEGORY_OPENABLE);
@@ -51,14 +66,14 @@ public class AlarmFileTest extends AppCompatActivity {
             }
         });
         saveButton = findViewById(R.id.save_button);
-        saveButton.setOnClickListener(new View.OnClickListener(){
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
-                Toast.makeText(getApplicationContext(),"파일 저장 버튼 클릭!", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "파일 저장 버튼 클릭!", Toast.LENGTH_SHORT).show();
                 Intent savefile = new Intent(Intent.ACTION_CREATE_DOCUMENT);
                 savefile.addCategory(Intent.CATEGORY_OPENABLE);
                 savefile.setType("*/*");
-                savefile.putExtra(Intent.EXTRA_TITLE,filenameView.getText().toString());
+                savefile.putExtra(Intent.EXTRA_TITLE, filenameView.getText().toString());
                 savefileLauncher.launch(savefile);
             }
         });
@@ -66,11 +81,38 @@ public class AlarmFileTest extends AppCompatActivity {
         alarmButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "알람 버튼 클릭!", Toast.LENGTH_SHORT).show();
+                sendNotification();
             }
         });
-
+        createNotificationChannel();
     }
+
+    public void createNotificationChannel() {
+        mNotificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(PRIMARY_CHANNEL_ID, "TESTNOTIFICIATION", mNotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notification from Mascot");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private NotificationCompat.Builder getNotificationBuilder() {
+        NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+                .setContentTitle("알림")
+                .setContentText("마감 기한이 얼마 안남았습니다.")
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp);//이미지 에셋 설정 --> file - new - imageasset으로 가 ic_android 설정해 newt - finish할것
+        return notifyBuilder;
+    }
+
+    public void sendNotification() {
+        NotificationCompat.Builder notifyBuilder = getNotificationBuilder();
+        mNotificationManager.notify(NOTIFICATION_ID, notifyBuilder.build());
+    }
+
     ActivityResultLauncher<Intent> openfileLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>()
             {
@@ -104,8 +146,5 @@ public class AlarmFileTest extends AppCompatActivity {
                     }
                 }
             });
-
-
-
 
 }
